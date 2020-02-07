2d95f704a270d5da501d07d83d834d9f6f59ccd0
Zeke <hnta3574@gmail.com>
2020-02-07 10:39:39 +0900

    max上で各オブジェクトにint型のパラメータを任意の数持たせる機能を追加

--- GameTemplate/tkToolsProj/maxPlugin/maxPlugin/maxPlugin.cpp
+++ GameTemplate/tkToolsProj/maxPlugin/maxPlugin/maxPlugin.cpp
@@ -149,6 +149,7 @@ enum {
 	pb_spin,
 	p_caster,
 	p_receiver,
+	p_intList
 };
 
 
@@ -167,6 +168,13 @@ static ParamBlockDesc2 maxplugin_param_blk ( maxplugin_params, _T("params"),  0,
 		p_default, TRUE,
 		p_ui, TYPE_SINGLECHECKBOX, IDC_CHECK2,
 	p_end,
+
+
+	p_intList, _T("intList"), TYPE_INT_TAB, 0, P_ANIMATABLE | P_VARIABLE_SIZE, 7,
+	p_range, 0, USHRT_MAX,
+	p_ui, TYPE_INTLISTBOX, IDC_LIST1, IDC_CUST_INT_ADD, IDC_CUST_INT_REPLACE, IDC_CUST_INT_DELETE, EDITTYPE_INT, IDC_CUST_INT_NEWVAL, IDCS_CUST_INT_NEWVAL, SPIN_AUTOSCALE,
+	p_end,
+
 	p_end
 	);
 
--- GameTemplate/tkToolsProj/maxPlugin/maxPlugin/resource.h
+++ GameTemplate/tkToolsProj/maxPlugin/maxPlugin/resource.h
@@ -8,12 +8,20 @@
 #define IDS_PARAMS                      4
 #define IDS_SPIN                        5
 #define IDS_CASTER                      6
+#define IDS_INTLIST                     7
 #define IDS_RECEIVER                    8
 #define IDD_PANEL                       101
 #define IDC_CLOSEBUTTON                 1000
 #define IDC_DOSTUFF                     1000
 #define IDC_CHECK1                      1001
 #define IDC_CHECK2                      1003
+#define IDC_LIST1                       1010
+#define IDC_CUST_INT_ADD                1012
+#define IDC_CUST_INT_REPLACE            1013
+#define IDC_CUST_INT_DELETE             1014
+#define IDC_CUST_INT_NEWVAL             1015
+#define IDC_CUSTOM3                     1016
+#define IDCS_CUST_INT_NEWVAL            1016
 #define IDC_COLOR                       1456
 #define IDC_EDIT                        1490
 #define IDC_EDIT2                       1491
@@ -26,7 +34,7 @@
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        102
 #define _APS_NEXT_COMMAND_VALUE         40001
-#define _APS_NEXT_CONTROL_VALUE         1004
+#define _APS_NEXT_CONTROL_VALUE         1017
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
