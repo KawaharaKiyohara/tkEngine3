ecee83f22f99bea67a20fa8b2d4c48e33f1f7203
TakayukiKiyohara <TakayukiKiyohara@gmail.com>
2020-02-18 21:35:44 +0900

    ほげ

--- GameTemplate/tkEngine/graphics/Dx12/tkPipelineStatesDx12.h
+++ GameTemplate/tkEngine/graphics/Dx12/tkPipelineStatesDx12.h
@@ -13,10 +13,15 @@ namespace tkEngine{
         static CShaderDx12 m_skinModelVS;		//スキンモデル用の頂点シェーダー。
         static CShaderDx12 m_skinModelPS;		//スキンモデル用の頂点シェーダー。
         static CShaderDx12 m_nonSkinModelVS;	//ノンスキンモデル用の頂点シェーダー。
+		static CShaderDx12 m_skinModelShadowMapVS;		//スキンモデルのシャドウマップ書き込み用の頂点シェーダー。
+		static CShaderDx12 m_nonSkinModelShadowMapVS;	//ノンスキンモデルのシャドウマップ書き込み用の頂点シェーダー。
+		static CShaderDx12 m_modelShadowMapPS;			//モデルのシャドウマップ書き込み用のピクセルシェーダー。
 		static CShaderDx12 m_spriteVS;			//スプライト描画用の頂点シェーダー。
 		static CShaderDx12 m_spritePS;			//スプライト描画用のピクセルシェーダー。
-        static ComPtr<ID3D12PipelineState> m_skinModelPipeline;		//スキンモデル描画用のパイプライン。
-        static ComPtr<ID3D12PipelineState> m_nonSkinModelPipeline;	//ノンスキンモデル用のパイプライン。
+        static ComPtr<ID3D12PipelineState> m_skinModelPipeline;			//スキンモデル描画用のパイプライン。
+        static ComPtr<ID3D12PipelineState> m_nonSkinModelPipeline;		//ノンスキンモデル用のパイプライン。
+		static ComPtr<ID3D12PipelineState> m_skinModelShadowMapPipeline;		//スキンモデルのシャドウマップ書き込み用のパイプライン。
+		static ComPtr<ID3D12PipelineState> m_nonSkinModelShadowMapPipeline;	//ノンスキンモデルのシャドウマップ書き込み用のパイプライン。
 		static ComPtr<ID3D12PipelineState> m_spritePipeline;		//スプライト用のパイプライン。
 		static ComPtr<ID3D12PipelineState> m_copyMainTargetToFrameBufferPipeline;	//メインレンダリングターゲットからフレームバッファにコピーする用のパイプライン。
 	private:
